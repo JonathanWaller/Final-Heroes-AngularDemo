e12db418a8c8d42de4f40c746bb9b33b5e4e87d8
JonathanWaller <waller.j27@gmail.com>
2019-06-23 19:44:16 -0500

    added message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
   
   // hero = 'Windstorm';
   
@@ -18,14 +24,21 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   
-  heroes = HEROES;
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  
+  // heroes = HEROES;
+  heroes: Hero[];
   
   // hero: Hero = {
   //   id: 1
   //   , name: 'Windstorm'
   // }
-
-  ngOnInit() {
-  }
 
 }
